f4a8438f3d4da5305481f16e596b87d931a939e6
Naveenraj003 <151421091+Naveenraj003@users.noreply.github.com>
2026-01-13 10:24:55 +0530

    Create binary_search.py

---
+++ 12.01.2026/binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    arr = list(map(int, input("Enter sorted array: ").split()))
+    target = int(input("Enter target: "))
+    index = binary_search(arr, target)
+
+    if index != -1:
+        print("Element found at index:", index)
+    else:
+        print("Element not found")
